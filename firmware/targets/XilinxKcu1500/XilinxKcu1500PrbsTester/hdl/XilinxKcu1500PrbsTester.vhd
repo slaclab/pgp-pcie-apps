cc9e94515cfa8940d98c98c55f4d53d5b25247b8
Larry Ruckman <ruckman@slac.stanford.edu>
2020-07-09 13:49:06 -0700

    bug fix for TID

--- firmware/targets/XilinxKcu1500/XilinxKcu1500PrbsTester/hdl/XilinxKcu1500PrbsTester.vhd
+++ firmware/targets/XilinxKcu1500/XilinxKcu1500PrbsTester/hdl/XilinxKcu1500PrbsTester.vhd
@@ -40,9 +40,9 @@ entity XilinxKcu1500PrbsTester is
       ROGUE_SIM_EN_G       : boolean                     := false;
       ROGUE_SIM_PORT_NUM_G : natural range 1024 to 49151 := 8000;
 
-      DMA_AXIS_CONFIG_G : AxiStreamConfigType := ssiAxiStreamConfig(8, TKEEP_COMP_C, TUSER_FIRST_LAST_C, 8, 2);  --- 8 Byte (64-bit) tData interface
-      -- DMA_AXIS_CONFIG_G : AxiStreamConfigType := ssiAxiStreamConfig(16, TKEEP_COMP_C, TUSER_FIRST_LAST_C, 8, 2);  --- 16 Byte (128-bit) tData interface
-      -- DMA_AXIS_CONFIG_G : AxiStreamConfigType := ssiAxiStreamConfig(32, TKEEP_COMP_C, TUSER_FIRST_LAST_C, 8, 2);  --- 32 Byte (256-bit) tData interface
+      DMA_AXIS_CONFIG_G : AxiStreamConfigType := ssiAxiStreamConfig(dataBytes => 8, tDestBits => 8, tIdBits => 8);  --- 8 Byte (64-bit) tData interface
+      -- DMA_AXIS_CONFIG_G : AxiStreamConfigType := ssiAxiStreamConfig(dataBytes => 16, tDestBits => 8, tIdBits => 8);  --- 16 Byte (128-bit) tData interface
+      -- DMA_AXIS_CONFIG_G : AxiStreamConfigType := ssiAxiStreamConfig(dataBytes => 32, tDestBits => 8, tIdBits => 8);  --- 32 Byte (256-bit) tData interface
 
       PRBS_SEED_SIZE_G : natural range 32 to 256 := 32;
 
