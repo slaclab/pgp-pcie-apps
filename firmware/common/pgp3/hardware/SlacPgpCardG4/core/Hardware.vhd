a69873212e8e98d6b3cf21d742c4e379ca642142
Larry Ruckman <ruckman@slac.stanford.edu>
2021-09-07 10:56:55 -0700

    bug fix for SlacPgpCardG4 + PGPv3/PGPv4

--- firmware/common/pgp3/hardware/SlacPgpCardG4/core/Hardware.vhd
+++ firmware/common/pgp3/hardware/SlacPgpCardG4/core/Hardware.vhd
@@ -116,7 +116,8 @@ begin
 
       U_QPLL : entity surf.Pgp3GthUsQpll
          generic map (
-            TPD_G => TPD_G)
+            TPD_G  => TPD_G,
+            RATE_G => RATE_G)
          port map (
             -- Stable Clock and Reset
             stableClk  => axilClk,
