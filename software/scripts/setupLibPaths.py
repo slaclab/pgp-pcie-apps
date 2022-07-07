1cd478a7ab5fd3b6bb87d299ce740bd360ce2a49
Benjamin Reese <bareese@slac.stanford.edu>
2022-07-07 12:15:10 -0700

    Add common firmware python to setupLibPaths.py

--- software/scripts/setupLibPaths.py
+++ software/scripts/setupLibPaths.py
@@ -14,3 +14,4 @@ top_level = os.path.realpath(__file__).split('software')[0]
 
 pr.addLibraryPath(top_level+'firmware/submodules/axi-pcie-core/python')
 pr.addLibraryPath(top_level+'firmware/submodules/surf/python')
+pr.addLibraryPath(top_level+'firmware/python')
