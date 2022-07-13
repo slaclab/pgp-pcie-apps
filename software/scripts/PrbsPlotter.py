fe85eefc5f5f8b460842f789b1cecdcd5aa4807d
Seth Kiefer <sethk@stanford.edu>
2022-07-13 13:28:39 -0700

    moved plt.show()

--- software/scripts/PrbsPlotter.py
+++ software/scripts/PrbsPlotter.py
@@ -14,7 +14,7 @@ zdata = 15 * np.random.random(100)
 xdata = np.sin(zdata) + 0.1 * np.random.randn(100)
 ydata = np.cos(zdata) + 0.1 * np.random.randn(100)
 ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens');
-plt.show()
+
 
 db_con = sqlite3.connect("~/pgp-pcie-apps/software/scripts/test3")
 cur = db_con.cursor()
@@ -22,4 +22,5 @@ statement = '''SELECT * FROM raw_data'''
 
 cur.execute(statement)
 rows = cur.fetchall()
-print(rows[0])
+print(rows[0:5])
+plt.show()
