175b93e9bca4d723e49998b6abb36dc91b966f0a
Seth Kiefer <sethk@stanford.edu>
2022-07-15 12:54:41 -0700

    changed to work with updated db

--- software/scripts/PrbsPlotter.py
+++ software/scripts/PrbsPlotter.py
@@ -35,19 +35,22 @@ def plotHzVsNumVc(db_con, args):
     # initialize arrays
     xdata = [[]*20 for i in range(20)]
     ydata = [[]*20 for i in range(20)]
-    ytotal = [[0]*7 for i in range(20)]
-    bwtot = [[0]*7 for i in range(20)]
+    ytotal = [[]*7 for i in range(20)]
+    bwtot = [[]*7 for i in range(20)]
     #yexpected = [[0]*7 for i in range(20)]
-    xtotals = [1.1, 2.1, 3.1, 4.1, 5.1, 6.1, 7.1]
+    xtotals = [1.1, 2.1, 3.1, 4.1, 5.1, 6.1, 7.1, 8.1]
 
     # collect data
     for rw in rows:
         if(rw[1]==19):
-            print(rw)
-            xdata[rw[2]].append(rw[0])
-            ydata[rw[2]].append(rw[4])
-            ytotal[rw[2]][rw[0]-1] += rw[4]
-            bwtot[rw[2]][rw[0]-1] += rw[3]
+            if(rw[0] == -1):
+                ytotal[rw[2]].append(rw[4])
+                bwtot[rw[2]].append(rw[3])
+            else:
+                print(rw)
+                xdata[rw[2]].append(rw[0])
+                ydata[rw[2]].append(rw[4])
+            
            # yexpected[rw[2]][rw[0]-1] += 48e9/((2**rw[2])*rw[0])
 
     for sets in range(20):
@@ -85,7 +88,7 @@ def plotBwVsNumVc(db_con, args):
 
     # prep data base for query
     cur = db_con.cursor()
-    statement = '''SELECT set_num_lanes, set_rate, set_packet_length, tx_bandwidth, tx_frame_rate FROM raw_data'''
+    statement = '''SELECT set_num_lanes, set_rate, set_packet_length, tx_bandwidth, tx_frame_rate, iteration_num FROM raw_data'''
 
     # get data from data base
     cur.execute(statement)
@@ -94,22 +97,23 @@ def plotBwVsNumVc(db_con, args):
     # initialize arrays
     xdata = [[]*20 for i in range(20)]
     ydata = [[]*20 for i in range(20)]
-    ytotal = [[0]*7 for i in range(20)]
-    bwtot = [[0]*7 for i in range(20)]
-    xtotals = [1.1, 2.1, 3.1, 4.1, 5.1, 6.1, 7.1]
+    bwtot = [[]*7 for i in range(20)]
+    xtotals = [1.1, 2.1, 3.1, 4.1, 5.1, 6.1, 7.1, 8.1]
 
     # collect data
     for rw in rows:
         if(rw[1]==19):
-            print(rw)
-            xdata[rw[2]].append(rw[0])
-            ydata[rw[2]].append(rw[3])
-            ytotal[rw[2]][rw[0]-1] += rw[3]
-            bwtot[rw[2]][rw[0]-1] += rw[3]
+            if(rw[0] == -1):
+                bwtot[rw[2]].append(rw[3])
+            else:
+                print(rw)
+                xdata[rw[2]].append(rw[0])
+                ydata[rw[2]].append(rw[3])
+                
 
     for sets in range(20):
         plt.plot(xdata[sets], ydata[sets], 'o', linestyle = 'solid', label = (sets+1))
-        plt.plot(xtotals, ytotal[sets], 'o', color = 'red', linestyle = 'dashed', label = (sets+1))
+        plt.plot(xtotals, bwtot[sets], 'o', color = 'red', linestyle = 'dashed', label = (sets+1))
         #plt.plot(xtotals, yexpected[sets], 'o', color = 'blue', linestyle = 'dashed', label = (sets+1))
     legend = plt.legend(loc = 'best')
 
@@ -145,7 +149,7 @@ args = parser.parse_args()
 
 zdata = []
 
-db_con = sqlite3.connect("test3")
+db_con = sqlite3.connect("test5")
 
 plotBwVsNumVc(db_con, args)
 
