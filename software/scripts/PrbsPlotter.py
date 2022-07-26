d7473de9f425f9bcc636cff084aec89cbae915b1
Seth Kiefer <sethk@stanford.edu>
2022-07-26 12:02:29 -0700

    updated plotter

--- software/scripts/PrbsPlotter.py
+++ software/scripts/PrbsPlotter.py
@@ -29,30 +29,77 @@ def plot3D(x, y, z):
 def calculateBandwidthError(packetLength, frameRate, bandwidth):
     return ((packetLength+1)*32*8)*frameRate/(bandwidth*1e6)
 
+def calculateExpectedRate(bandwidth, packetLength):
+    return 0 #channels*lanes/((packetLength+1)4e-9) #not if bandwidth cap is reached
+
 def prepDicts(index, dicts):
     for d in dicts:
         if index not in d:
-            d[index] = []
+            d[index] = {}
 
 def queryData(db_con):
     # prep data base for query
     cur = db_con.cursor()
-    statement = '''SELECT set_num_lanes, set_num_vc, set_packet_length, tx_bandwidth, tx_frame_rate, tx_bandwidth_max, tx_frame_rate_max, tx_bandwidth_min, tx_frame_rate_min, lane FROM raw_data'''
+    statement = '''SELECT set_num_lanes, set_num_vc, set_packet_length, tx_bandwidth, tx_frame_rate, tx_bandwidth_max, tx_frame_rate_max, tx_bandwidth_min, tx_frame_rate_min, lane, channel FROM raw_data'''
 
     # get data from data base
     ret = cur.execute(statement)
     return ret
 
-def displayFromDicts(
-    x, 
-    y, 
-    total, 
-    tOffSet,
+def flowSelect(data, aggregate, barData, barAggregate, yHigh, yLow, namer, displayFilter, displayMax, displayError, graphType):
+    
+    passedData = aggregate if(displayMax) else data
+    
+    # display data 
+    if(graphType == 'scatter'):
+        
+        passedData = aggregate if(displayMax) else data
+        displayFromDictsScatter(passedData, yHigh, yLow, namer, displayError, len(passedData)) 
+        
+    elif(graphType == 'bar'):
+        passedData = barAggregate if(displayMax) else barData
+        displayFromDictsBar(passedData, namer, len(passedData)) 
+        
+    else:
+        print("invalid graph type")
+    
+    
+
+def displayFromDictsBar(
+    data, 
+    namer, 
+    colorCount
+):
+
+    colormap = plt.get_cmap('gist_rainbow')
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_prop_cycle('color', plt.cm.Spectral(np.linspace(0,1,colorCount)))
+    
+    spacer = 0
+    width = .05
+    
+    # display all rows
+    for sets in data: 
+        
+        sortedData = dict(sorted(data[sets].items(), key=lambda k: k[0][0]*k[0][1]))
+        
+        xaxis = np.arange(len(sortedData))
+                    
+        plt.bar(xaxis+width*(spacer - len(data)/2), list(sortedData.values()), width = width, label = namer(sets))
+
+        spacer +=1
+    
+        plt.xticks(np.arange(len(sortedData)), sortedData.keys())
+                
+        
+    
+                
+def displayFromDictsScatter(
+    data, 
     yhigh,
     ylow,
     namer, 
-    displayFilter, 
-    displayMax,
     displayError,
     colorCount
 ):
@@ -63,17 +110,14 @@ def displayFromDicts(
     ax.set_prop_cycle('color', plt.cm.Spectral(np.linspace(0,1,colorCount)))
     
     # display all rows
-    for sets in x:
-        if(displayFilter(y[sets], total[sets], sets)):
-            if(not displayMax):
-                plt.plot(x[sets], y[sets], 'o', linestyle = 'solid', label = namer(sets))
-                if(displayError):
-                    color = next(ax._get_lines.prop_cycler)['color']
-                    plt.plot(x[sets], yhigh[sets], '1', markersize = 15, color = color)
-                    plt.plot(x[sets], ylow[sets], '2', markersize = 15, color = color)
-            else:
-                plt.plot(tOffSet[sets], total[sets], 'o', linestyle = 'dashed', label = str(namer(sets)))
+    for sets in data:            
+        plt.plot(list(data[sets].keys()), list(data[sets].values()), 'o', linestyle = 'solid', label = namer(sets))
 
+        if(displayError):
+            color = next(ax._get_lines.prop_cycler)['color']
+            plt.plot(list(data[sets].keys()), list(yhigh[sets].values()), '1', markersize = 15, color = color)
+            plt.plot(list(data[sets].keys()), list(ylow[sets].values()), '2', markersize = 15, color = color)
+                    
 
 def collectDataVsFrameSize(
     rows, 
@@ -82,45 +126,45 @@ def collectDataVsFrameSize(
     namer, 
     displayFilter, 
     displayMax, 
-    displayError
+    displayError,
+    graphType
 ):
     
     # initialize dicts
-    xdata   = {}
-    ydata   = {}
+    data    = {}
+    tot     = {}
+    barData = {}
+    barTot  = {}
+    
     yhigh   = {}
     ylow    = {}
-    ytot    = {}
-    xtot    = {}
 
     # collect data
     for rw in rows:
 
-        index = (rw[0], rw[1])
+        indexA = math.log2(rw[2])
+        indexB = (rw[0], rw[1])
 
         if(collectionFilter(rw)):
 
-            prepDicts(index,[xdata, ydata, yhigh, ylow, ytot, xtot])
-
+            prepDicts(indexB,[data, tot, yhigh, ylow])
+            
             if(rw[9] == -1):
-                ytot[index].append(rw[dataIndex])
-                xtot[index].append(math.log2(rw[2]))
+                tot[indexB][indexA] = rw[dataIndex]
 
             elif(dataIndex == 5):
-                xdata[index].append(math.log2(rw[2]))
-                ydata[index].append(calculateBandwidthError(rw[2], rw[4], rw[3]))
+                data[indexB][indexA] = calculateBandwidthError(rw[2], rw[4], rw[3])
                 
             else:
-                xdata[index].append(math.log2(rw[2]))
-                ydata[index].append(rw[dataIndex])
+                data[indexB][indexA] = rw[dataIndex]
                 
-                yhigh[index].append(rw[dataIndex+2])
-                ylow[index].append(rw[dataIndex+4])
+                yhigh[indexB][indexA] = rw[dataIndex+2]
+                ylow[indexB][indexA] = rw[dataIndex+4]
                 
                 
 
-    # display data            
-    displayFromDicts(xdata, ydata, ytot, xtot, yhigh, ylow, namer, displayFilter, displayMax, displayError, 8) 
+    # display data 
+    flowSelect(data, tot, barData, barTot, yhigh, ylow, namer, displayFilter, displayMax, displayError, graphType)
 
     # set x axis label
     plt.xlabel("log2 of frame size") 
@@ -132,40 +176,44 @@ def collectDataVsVc(
     namer, 
     displayFilter, 
     displayMax, 
-    displayError
+    displayError,
+    graphType
 ):
     
     # initialize dicts
-    xdata   = {}
-    ydata   = {}
+    data    = {}
+    tot     = {}
+    barData = {}
+    barTot  = {}
+    
     yhigh   = {}
     ylow    = {}
-    ytot    = {}
-    xtot    = {}
+    
 
 
     # collect data
     for rw in rows:
 
-        index = (int(math.log2(rw[2]))-1)
+        indexA = (int(math.log2(rw[2])))
+        indexB = (rw[0], rw[1])
 
         if(collectionFilter(rw)):
 
-            prepDicts(index,[xdata, ydata, yhigh, ylow, ytot, xtot])
+            prepDicts(indexB,[data, tot])
+            prepDicts(indexA,[barData, barTot])
 
             if(rw[9] == -1):
-                ytot[index].append(rw[dataIndex])
-                xtot[index].append(rw[0]*rw[1])
+                barTot[indexA][indexB] = rw[dataIndex]
+                tot[indexB][indexA] = rw[dataIndex]
                 
             else:
-                xdata[index].append(rw[0])
-                ydata[index].append(rw[dataIndex])
+                barData[indexA][indexB] = rw[dataIndex]
+                data[indexB][indexA] = rw[dataIndex]
                 
-                yhigh[index].append(rw[dataIndex+2])
-                ylow[index].append(rw[dataIndex+4])
+                #yhigh[index].append(rw[dataIndex+2])
+                #ylow[index].append(rw[dataIndex+4])
 
-    # display data            
-    displayFromDicts(xdata, ydata, ytot, xtot, yhigh, ylow, namer, displayFilter, displayMax, displayError, 20) 
+    flowSelect(data, tot, barData, barTot, yhigh, ylow, namer, displayFilter, displayMax, displayError, graphType)
 
     # set x axis label
     plt.xlabel("Active Channels")
@@ -179,17 +227,18 @@ def plotData(
     displayFilter = lambda val, maxi, index: True, 
     namer = lambda num: 2**(num+1), 
     legendTitle = 'Frame size in words',
-    displayError = False
+    displayError = False,
+    graphType = 'scatter'
 ):
 
     # query sql data base
     rows = queryData(db_con)
 
-    # collect and dispaly data
+    # collect and display data
     if(funcSelect == 1):
-        collectDataVsVc(rows, dataIndex, collectionFilter, namer, displayFilter, displayAggregate, displayError)
+        collectDataVsVc(rows, dataIndex, collectionFilter, namer, displayFilter, displayAggregate, displayError, graphType)
     elif(funcSelect == 2):
-        collectDataVsFrameSize(rows, dataIndex, collectionFilter, namer, displayFilter, displayAggregate, displayError)
+        collectDataVsFrameSize(rows, dataIndex, collectionFilter, namer, displayFilter, displayAggregate, displayError, graphType)
     else:
         print("invalid function selection")
     
