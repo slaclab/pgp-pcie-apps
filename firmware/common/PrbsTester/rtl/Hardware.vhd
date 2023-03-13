b935e342b9fe5b0f72ec052a2b36962461d2af1c
Benjamin Reese <bareese@slac.stanford.edu>
2023-03-13 07:42:19 -0700

    Add axi-lite pipeline stage to make timing

--- firmware/common/PrbsTester/rtl/Hardware.vhd
+++ firmware/common/PrbsTester/rtl/Hardware.vhd
@@ -91,7 +91,7 @@ begin
          TPD_G                => TPD_G,
          NUM_SLAVE_SLOTS_G    => 1,
          NUM_MASTER_SLOTS_G   => 9,
-         MASTER_PIPE_STAGES_G => 1,
+         MASTER_PIPE_STAGES_G => 2,
          MASTERS_CONFIG_G     => AXI_CONFIG_C)
       port map (
          axiClk              => axilClk,
