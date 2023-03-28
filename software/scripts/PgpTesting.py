bbbcd69e6a4d2d3c727c80b174b9b4fc3db06787
Benjamin Reese <bareese@slac.stanford.edu>
2023-03-28 10:14:24 -0700

    Cleanup command line arguments

--- software/scripts/PgpTesting.py
+++ software/scripts/PgpTesting.py
@@ -30,8 +30,6 @@ import surf.protocols.pgp as pgp
 # Set the argument parser
 parser = argparse.ArgumentParser()
 
-# Convert str to bool
-argBool = lambda s: s.lower() in ['true', 't', 'yes', '1']
 
 # Add arguments
 parser.add_argument(
@@ -43,11 +41,19 @@ parser.add_argument(
 )
 
 parser.add_argument(
-    "--numLane",
+    "--dmaLanes",
     type     = int,
     required = False,
     default  = 1,
     help     = "# of DMA Lanes",
+)
+
+parser.add_argument(
+    "--pgpLanes",
+    type     = int,
+    required = False,
+    default  = 1,
+    help     = "# of PGP Lanes",
 )
 
 parser.add_argument(
@@ -68,18 +74,16 @@ parser.add_argument(
 
 parser.add_argument(
     "--swRx",
-    type     = argBool,
-    required = False,
-    default  = True,
-    help     = "Enable read all variables at start",
+    action = 'store_true',
+    default  = False,
+    help     = "Add PRBS Rx to tree"
 )
 
 parser.add_argument(
     "--swTx",
-    type     = argBool,
-    required = False,
-    default  = True,
-    help     = "Enable read all variables at start",
+    action ='store_true',
+    default  = False,
+    help     = "Add PRBS Tx to tree"
 )
 
 parser.add_argument(
@@ -87,22 +91,20 @@ parser.add_argument(
     type     = int,
     required = False,
     default  = 256,
-    help     = "# of DMA Lanes",
+    help     = "Word with for PRBS software modules"
 )
 
 parser.add_argument(
     "--pollEn",
-    type     = argBool,
-    required = False,
-    default  = True,
+    action = 'store_true',
+    default  = False,
     help     = "Enable auto-polling",
 )
 
 parser.add_argument(
     "--initRead",
-    type     = argBool,
-    required = False,
-    default  = True,
+    action = 'store_true',
+    default  = False,
     help     = "Enable read all variables at start",
 )
 
@@ -120,35 +122,36 @@ args = parser.parse_args()
 #################################################################
 
 class MyRoot(pr.Root):
-    def __init__(   self,
-            name        = "pciServer",
-            description = "DMA Loopback Testing",
-            **kwargs):
+    def __init__(self,
+                 name        = "pciServer",
+                 description = "DMA Loopback Testing",
+                 **kwargs):
+    
         super().__init__(name=name, description=description, **kwargs)
 
         # Create PCIE memory mapped interface
         self.memMap = rogue.hardware.axi.AxiMemMap(args.dev)
 
         if (args.swRx or args.swTx):
-            self.dmaStream   = [[None for x in range(args.numVc)] for y in range(args.numLane)]
-            self.prbsRx      = [[None for x in range(args.numVc)] for y in range(args.numLane)]
-            self.prbsTx      = [[None for x in range(args.numVc)] for y in range(args.numLane)]
+            self.dmaStream   = [[None for x in range(args.numVc)] for y in range(args.pgpLanes)]
+            self.prbsRx      = [[None for x in range(args.numVc)] for y in range(args.pgpLanes)]
+            self.prbsTx      = [[None for x in range(args.numVc)] for y in range(args.pgpLanes)]
 
         # Add the PCIe core device to base
         self.add(pcie.AxiPcieCore(
             offset     = 0x00000000,
             memBase     = self.memMap,
-            numDmaLanes = args.numLane,
+            numDmaLanes = args.dmaLanes,
             boardType   = args.boardType,
             expand      = True,
         ))
 
         # Add PGP Core
-        for lane in range(args.numLane):
+        for pgpLane in range(args.pgpLanes):
             if (args.version == 4):
                 self.add(pgp.Pgp4AxiL(
-                    name    = f'Lane[{lane}]',
-                    offset  = (0x00800000 + lane*0x00010000),
+                    name    = f'Lane[{pgpLane}]',
+                    offset  = (0x00800000 + pgpLane*0x00010000),
                     memBase = self.memMap,
                     numVc   = args.numVc,
                     writeEn = True,
@@ -156,8 +159,8 @@ class MyRoot(pr.Root):
                 ))
             elif (args.version == 3):
                 self.add(pgp.Pgp3AxiL(
-                    name    = f'Lane[{lane}]',
-                    offset  = (0x00800000 + lane*0x00010000),
+                    name    = f'Lane[{pgpLane}]',
+                    offset  = (0x00800000 + pgpLane*0x00010000),
                     memBase = self.memMap,
                     numVc   = args.numVc,
                     writeEn = True,
@@ -165,56 +168,60 @@ class MyRoot(pr.Root):
                 ))
             else:
                 self.add(pgp.Pgp2bAxi(
-                    name    = f'Lane[{lane}]',
-                    offset  = (0x00800000 + lane*0x00010000 + 0x1000),
+                    name    = f'Lane[{pgpLane}]',
+                    offset  = (0x00800000 + pgpLane*0x00010000 + 0x1000),
                     memBase = self.memMap,
                     expand  = False,
                 ))
 
             self.add(axi.AxiStreamMonAxiL(
-                name        = (f'PgpTxAxisMon[{lane}]'),
-                offset      = (0x00800000 + lane*0x00010000 + 0x3000),
+                name        = (f'PgpTxAxisMon[{pgpLane}]'),
+                offset      = (0x00800000 + pgpLane*0x00010000 + 0x3000),
                 numberLanes = args.numVc,
                 memBase     = self.memMap,
                 expand      = False,
             ))
 
             self.add(axi.AxiStreamMonAxiL(
-                name        = (f'PgpRxAxisMon[{lane}]'),
-                offset      = (0x00800000 + lane*0x00010000 + 0x4000),
+                name        = (f'PgpRxAxisMon[{pgpLane}]'),
+                offset      = (0x00800000 + pgpLane*0x00010000 + 0x4000),
                 numberLanes = args.numVc,
                 memBase     = self.memMap,
                 expand      = False,
             ))
 
+            pgpLanesPerDmaLane = args.pgpLanes // args.dmaLanes
+
             # Loop through the virtual channels
             for vc in range(args.numVc):
 
+                dmaDest = ((pgpLane // pgpLanesPerDmaLane) * 0x100) + ((pgpLane % pgpLanesPerDmaLane) * 0b100) + vc
+
                 if (args.swRx or args.swTx):
-                    self.dmaStream[lane][vc] = rogue.hardware.axi.AxiStreamDma(args.dev,(0x100*lane)+vc,1)
+                    self.dmaStream[pgpLane][vc] = rogue.hardware.axi.AxiStreamDma(args.dev, dmaDest, 1)
 
                 if (args.swRx):
 
                     # Connect the SW PRBS Receiver module
-                    self.prbsRx[lane][vc] = pr.utilities.prbs.PrbsRx(
-                        name         = ('SwPrbsRx[%d][%d]'%(lane,vc)),
+                    self.prbsRx[pgpLane][vc] = pr.utilities.prbs.PrbsRx(
+                        name         = f'SwPrbsRx[{pgpLane}][{vc}]',
                         width        = args.prbsWidth,
                         checkPayload = True,
                         expand       = False,
                     )
-                    self.dmaStream[lane][vc] >> self.prbsRx[lane][vc]
-                    self.add(self.prbsRx[lane][vc])
+                    self.dmaStream[pgpLane][vc] >> self.prbsRx[pgpLane][vc]
+                    self.add(self.prbsRx[pgpLane][vc])
 
                 if (args.swTx):
 
                     # Connect the SW PRBS Transmitter module
-                    self.prbsTx[lane][vc] = pr.utilities.prbs.PrbsTx(
-                        name    = ('SwPrbsTx[%d][%d]'%(lane,vc)),
+                    self.prbsTx[pgpLane][vc] = pr.utilities.prbs.PrbsTx(
+                        name    = f'SwPrbsTx[{pgpLane}][{vc}]',
                         width   = args.prbsWidth,
                         expand  = False,
                     )
-                    self.prbsTx[lane][vc] >> self.dmaStream[lane][vc]
-                    self.add(self.prbsTx[lane][vc])
+                    self.prbsTx[pgpLane][vc] >> self.dmaStream[pgpLane][vc]
+                    self.add(self.prbsTx[pgpLane][vc])
 
         @self.command()
         def EnableAllSwTx():
