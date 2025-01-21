803f6fde0cc79c26cfc403d88e63c58057df5a36
Julian Mendez <jumdz@slac.stanford.edu>
2025-01-21 05:39:02 -0800

    add eye diag scripts

--- firmware/python/pgpapps/__init__.py
+++ firmware/python/pgpapps/__init__.py
@@ -1,17 +1,10 @@
 ##############################################################################
-## This file is part of 'PGP PCIe APP DEV'.
+## This file is part of 'SLAC Firmware Standard Library'.
 ## It is subject to the license terms in the LICENSE.txt file found in the
 ## top-level directory of this distribution and at:
 ##    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
-## No part of 'PGP PCIe APP DEV', including this file,
+## No part of 'SLAC Firmware Standard Library', including this file,
 ## may be copied, modified, propagated, or distributed except according to
 ## the terms contained in the LICENSE.txt file.
 ##############################################################################
-import pyrogue as pr
-import os
-
-top_level = os.path.realpath(__file__).split('software')[0]
-
-pr.addLibraryPath(top_level+'firmware/submodules/axi-pcie-core/python')
-pr.addLibraryPath(top_level+'firmware/submodules/surf/python')
-pr.addLibraryPath(top_level+'firmware/python')
+from pgpapps._EyeGty import *
---
+++ software/scripts/EyeDiag.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+##############################################################################
+## This file is part of 'PGP PCIe APP DEV'.
+## It is subject to the license terms in the LICENSE.txt file found in the
+## top-level directory of this distribution and at:
+##    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+## No part of 'PGP PCIe APP DEV', including this file,
+## may be copied, modified, propagated, or distributed except according to
+## the terms contained in the LICENSE.txt file.
+##############################################################################
+
+import setupLibPaths
+import sys
+import rogue
+import argparse
+
+import rogue.hardware.axi
+import rogue.interfaces.stream
+
+import pyrogue as pr
+import pyrogue.pydm
+import pyrogue.utilities.prbs
+
+import axipcie            as pcie
+import surf.axi           as axi
+import surf.protocols.pgp as pgp
+
+import pgpapps            as pgpapps
+
+#rogue.Logging.setLevel(rogue.Logging.Debug)
+
+#################################################################
+
+# Set the argument parser
+parser = argparse.ArgumentParser()
+
+# Convert str to bool
+argBool = lambda s: s.lower() in ['true', 't', 'yes', '1']
+
+# Add arguments
+parser.add_argument(
+    "--dev",
+    type     = str,
+    required = False,
+    default  = '/dev/datadev_0',
+    help     = "path to device",
+)
+
+parser.add_argument(
+    "--numLane",
+    type     = int,
+    required = False,
+    default  = 4,
+    help     = "# of DMA Lanes",
+)
+
+parser.add_argument(
+    "--numVc",
+    type     = int,
+    required = False,
+    default  = 4,
+    help     = "# of VC (virtual channels)",
+)
+
+parser.add_argument(
+    "--version",
+    type     = int,
+    required = False,
+    default  = 4,
+    help     = "PGP Protocol Version",
+)
+
+parser.add_argument(
+    "--pollEn",
+    type     = argBool,
+    required = False,
+    default  = True,
+    help     = "Enable auto-polling",
+)
+
+parser.add_argument(
+    "--initRead",
+    type     = argBool,
+    required = False,
+    default  = True,
+    help     = "Enable read all variables at start",
+)
+
+parser.add_argument(
+    "--boardType",
+    type     = str,
+    required = False,
+    default  = None,
+    help     = "define the type of PCIe card, used to select I2C mapping. Options: [none or SlacPgpCardG4, Kcu1500, etc]",
+)
+
+# Get the arguments
+args = parser.parse_args()
+
+#################################################################
+
+class MyRoot(pr.Root):
+    def __init__(   self,
+            name        = "pciServer",
+            description = "DMA Loopback Testing",
+            **kwargs):
+        super().__init__(name=name, description=description, **kwargs)
+
+        self.zmqServer = pyrogue.interfaces.ZmqServer(root=self, addr='127.0.0.1', port=0)
+        self.addInterface(self.zmqServer)
+
+        # Create PCIE memory mapped interface
+        self.memMap = rogue.hardware.axi.AxiMemMap(args.dev)
+
+        # Add the PCIe core device to base
+        self.add(pcie.AxiPcieCore(
+            offset     = 0x00000000,
+            memBase     = self.memMap,
+            numDmaLanes = args.numLane,
+            boardType   = args.boardType,
+            expand      = True,
+        ))
+
+        # Add PGP Core
+        for lane in range(args.numLane):
+            self.add(pgpapps.EyeGty(
+                memBase = self.memMap,
+                name   = "Eye[{}]".format(lane),
+                offset = (0x00800000 + lane*0x00010000 + 0x1000),
+            ))
+            
+            self.add(pgp.Pgp4AxiL(
+                name    = f'Lane[{lane}]',
+                offset  = (0x00800000 + lane*0x00010000),
+                memBase = self.memMap,
+                numVc   = args.numVc,
+                writeEn = True,
+                expand  = True,
+            ))
+            
+
+#################################################################
+
+with MyRoot(pollEn=args.pollEn, initRead=args.initRead) as root:
+     pyrogue.pydm.runPyDM(serverList = root.zmqServer.address)
+     #root.Lane[0].eyePlot(target=1e8)
+
+#################################################################
