72dad4e5c60f39140b8b107092f07a876d6ff5ed
Larry Ruckman <ruckman@slac.stanford.edu>
2025-03-21 14:51:41 -0700

    updating setup script

--- software/setup_env_slac.sh
+++ software/setup_env_slac.sh
@@ -1,9 +1,9 @@
 ##################################
 # Setup environment
 ##################################
-source /sdf/group/faders/tools/anaconda3/etc/profile.d/conda.sh
+source /sdf/group/faders/users/$USER/anaconda3/etc/profile.d/conda.sh
 
 ##################################
 # Activate Rogue conda Environment
 ##################################
-conda activate rogue_v6.4.4
+conda activate rogue_v6.5.0
