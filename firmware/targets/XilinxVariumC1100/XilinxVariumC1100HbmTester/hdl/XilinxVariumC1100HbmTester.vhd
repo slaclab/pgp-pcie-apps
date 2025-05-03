c69b2b82dab6d90ab95a8d34661d57dcff869815
Larry Ruckman <ruckman@slac.stanford.edu>
2025-05-03 13:40:48 -0700

    adding XilinxVariumC1100HbmTester target

---
+++ firmware/targets/XilinxVariumC1100/XilinxVariumC1100HbmTester/hdl/XilinxVariumC1100HbmTester.vhd
@@ -0,0 +1,268 @@
+-------------------------------------------------------------------------------
+-- Company    : SLAC National Accelerator Laboratory
+-------------------------------------------------------------------------------
+-- Description:
+-------------------------------------------------------------------------------
+-- This file is part of 'PGP PCIe APP DEV'.
+-- It is subject to the license terms in the LICENSE.txt file found in the
+-- top-level directory of this distribution and at:
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+-- No part of 'PGP PCIe APP DEV', including this file,
+-- may be copied, modified, propagated, or distributed except according to
+-- the terms contained in the LICENSE.txt file.
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+library surf;
+use surf.StdRtlPkg.all;
+use surf.AxiPkg.all;
+use surf.AxiLitePkg.all;
+use surf.AxiStreamPkg.all;
+use surf.SsiPkg.all;
+
+library axi_pcie_core;
+use axi_pcie_core.AxiPciePkg.all;
+
+library unisim;
+use unisim.vcomponents.all;
+
+entity XilinxVariumC1100HbmTester is
+   generic (
+      TPD_G        : time := 1 ns;
+      BUILD_INFO_G : BuildInfoType);
+   port (
+      ---------------------
+      --  Application Ports
+      ---------------------
+      -- HBM Ports
+      hbmCatTrip : out   sl;  -- HBM Catastrophic Over temperature Output signal to Satellite Controller: active HIGH indicator to Satellite controller to indicate the HBM has exceeds its maximum allowable temperature
+      --------------
+      --  Core Ports
+      --------------
+      -- Card Management Solution (CMS) Interface
+      cmsUartRxd : in    sl;
+      cmsUartTxd : out   sl;
+      cmsGpio    : in    slv(3 downto 0);
+      -- System Ports
+      userClkP   : in    sl;
+      userClkN   : in    sl;
+      hbmRefClkP : in    sl;
+      hbmRefClkN : in    sl;
+      -- SI5394 Ports
+      si5394Scl  : inout sl;
+      si5394Sda  : inout sl;
+      si5394IrqL : in    sl;
+      si5394LolL : in    sl;
+      si5394LosL : in    sl;
+      si5394RstL : out   sl;
+      -- PCIe Ports
+      pciRstL    : in    sl;
+      pciRefClkP : in    slv(0 downto 0);
+      pciRefClkN : in    slv(0 downto 0);
+      pciRxP     : in    slv(7 downto 0);
+      pciRxN     : in    slv(7 downto 0);
+      pciTxP     : out   slv(7 downto 0);
+      pciTxN     : out   slv(7 downto 0));
+end XilinxVariumC1100HbmTester;
+
+architecture top_level of XilinxVariumC1100HbmTester is
+
+   constant DMA_SIZE_C : positive := 2;
+
+   -- constant DMA_AXIS_CONFIG_C : AxiStreamConfigType := ssiAxiStreamConfig(dataBytes => 8, tDestBits => 8, tIdBits => 3);  -- 8  Byte (64-bit)  tData interface
+   -- constant DMA_AXIS_CONFIG_C : AxiStreamConfigType := ssiAxiStreamConfig(dataBytes => 16, tDestBits => 8, tIdBits => 3);  -- 16 Byte (128-bit) tData interface
+   -- constant DMA_AXIS_CONFIG_C : AxiStreamConfigType := ssiAxiStreamConfig(dataBytes => 32, tDestBits => 8, tIdBits => 3);  -- 32 Byte (256-bit) tData interface
+   constant DMA_AXIS_CONFIG_C : AxiStreamConfigType := ssiAxiStreamConfig(dataBytes => 64, tDestBits => 8, tIdBits => 3);  -- 64 Byte (512-bit) tData interface
+
+   constant AXIL_XBAR_CONFIG_C : AxiLiteCrossbarMasterConfigArray(4 downto 0) := (
+      0               => (
+         baseAddr     => x"0010_0000",
+         addrBits     => 20,
+         connectivity => x"FFFF"),
+      1               => (
+         baseAddr     => x"0020_0000",
+         addrBits     => 20,
+         connectivity => x"FFFF"),
+      2               => (
+         baseAddr     => x"0030_0000",
+         addrBits     => 20,
+         connectivity => x"FFFF"),
+      3               => (
+         baseAddr     => x"0040_0000",
+         addrBits     => 20,
+         connectivity => x"FFFF"),
+      4               => (
+         baseAddr     => x"0080_0000",
+         addrBits     => 23,
+         connectivity => x"FFFF"));
+
+   signal axilReadMaster  : AxiLiteReadMasterType;
+   signal axilReadSlave   : AxiLiteReadSlaveType;
+   signal axilWriteMaster : AxiLiteWriteMasterType;
+   signal axilWriteSlave  : AxiLiteWriteSlaveType;
+
+   signal axilReadMasters  : AxiLiteReadMasterArray(4 downto 0);
+   signal axilReadSlaves   : AxiLiteReadSlaveArray(4 downto 0)  := (others => AXI_LITE_READ_SLAVE_EMPTY_SLVERR_C);
+   signal axilWriteMasters : AxiLiteWriteMasterArray(4 downto 0);
+   signal axilWriteSlaves  : AxiLiteWriteSlaveArray(4 downto 0) := (others => AXI_LITE_WRITE_SLAVE_EMPTY_SLVERR_C);
+
+   signal dmaClk          : sl;
+   signal dmaRst          : sl;
+   signal dmaBuffGrpPause : slv(7 downto 0);
+   signal dmaObMasters    : AxiStreamMasterArray(DMA_SIZE_C-1 downto 0) := (others => AXI_STREAM_MASTER_INIT_C);
+   signal dmaObSlaves     : AxiStreamSlaveArray(DMA_SIZE_C-1 downto 0)  := (others => AXI_STREAM_SLAVE_FORCE_C);
+   signal dmaIbMasters    : AxiStreamMasterArray(DMA_SIZE_C-1 downto 0) := (others => AXI_STREAM_MASTER_INIT_C);
+   signal dmaIbSlaves     : AxiStreamSlaveArray(DMA_SIZE_C-1 downto 0)  := (others => AXI_STREAM_SLAVE_FORCE_C);
+   signal buffIbMasters   : AxiStreamMasterArray(DMA_SIZE_C-1 downto 0) := (others => AXI_STREAM_MASTER_INIT_C);
+   signal buffIbSlaves    : AxiStreamSlaveArray(DMA_SIZE_C-1 downto 0)  := (others => AXI_STREAM_SLAVE_FORCE_C);
+
+   signal hbmRefClk : sl;
+   signal userClk   : sl;
+
+   signal cmsHbmCatTrip : sl                    := '0';
+   signal cmsHbmTemp    : Slv7Array(1 downto 0) := (others => b"0000000");
+
+begin
+
+   U_Core : entity axi_pcie_core.XilinxVariumC1100Core
+      generic map (
+         TPD_G              => TPD_G,
+         QSFP_CDR_DISABLE_G => true,
+         BUILD_INFO_G       => BUILD_INFO_G,
+         DMA_AXIS_CONFIG_G  => DMA_AXIS_CONFIG_C,
+         DMA_SIZE_G         => 1)
+      port map (
+         ------------------------
+         --  Top Level Interfaces
+         ------------------------
+         userClk         => userClk,
+         hbmRefClk       => hbmRefClk,
+         -- DMA Interfaces
+         dmaClk          => dmaClk,
+         dmaRst          => dmaRst,
+         dmaBuffGrpPause => dmaBuffGrpPause,
+         dmaObMasters    => open,
+         dmaObSlaves     => (others => AXI_STREAM_SLAVE_FORCE_C),
+         dmaIbMasters    => (others => AXI_STREAM_MASTER_INIT_C),
+         dmaIbSlaves     => open,
+         -- Application AXI-Lite Interfaces [0x00100000:0x00FFFFFF]
+         appClk          => dmaClk,
+         appRst          => dmaRst,
+         appReadMaster   => axilReadMaster,
+         appReadSlave    => axilReadSlave,
+         appWriteMaster  => axilWriteMaster,
+         appWriteSlave   => axilWriteSlave,
+         --------------
+         --  Core Ports
+         --------------
+         -- Card Management Solution (CMS) Interface
+         cmsHbmCatTrip   => cmsHbmCatTrip,
+         cmsHbmTemp      => cmsHbmTemp,
+         cmsUartRxd      => cmsUartRxd,
+         cmsUartTxd      => cmsUartTxd,
+         cmsGpio         => cmsGpio,
+         -- System Ports
+         userClkP        => userClkP,
+         userClkN        => userClkN,
+         hbmRefClkP      => hbmRefClkP,
+         hbmRefClkN      => hbmRefClkN,
+         -- SI5394 Ports
+         si5394Scl       => si5394Scl,
+         si5394Sda       => si5394Sda,
+         si5394IrqL      => si5394IrqL,
+         si5394LolL      => si5394LolL,
+         si5394LosL      => si5394LosL,
+         si5394RstL      => si5394RstL,
+         -- PCIe Ports
+         pciRstL         => pciRstL,
+         pciRefClkP      => pciRefClkP,
+         pciRefClkN      => pciRefClkN,
+         pciRxP          => pciRxP,
+         pciRxN          => pciRxN,
+         pciTxP          => pciTxP,
+         pciTxN          => pciTxN);
+
+   --------------------
+   -- AXI-Lite Crossbar
+   --------------------
+   U_XBAR : entity surf.AxiLiteCrossbar
+      generic map (
+         TPD_G              => TPD_G,
+         NUM_SLAVE_SLOTS_G  => 1,
+         NUM_MASTER_SLOTS_G => 5,
+         MASTERS_CONFIG_G   => AXIL_XBAR_CONFIG_C)
+      port map (
+         axiClk              => dmaClk,
+         axiClkRst           => dmaRst,
+         sAxiWriteMasters(0) => axilWriteMaster,
+         sAxiWriteSlaves(0)  => axilWriteSlave,
+         sAxiReadMasters(0)  => axilReadMaster,
+         sAxiReadSlaves(0)   => axilReadSlave,
+         mAxiWriteMasters    => axilWriteMasters,
+         mAxiWriteSlaves     => axilWriteSlaves,
+         mAxiReadMasters     => axilReadMasters,
+         mAxiReadSlaves      => axilReadSlaves);
+
+   ----------------------------
+   -- DMA Inbound Large Buffer
+   ----------------------------
+   U_HbmDmaBuffer : entity axi_pcie_core.HbmDmaBuffer
+      generic map (
+         TPD_G             => TPD_G,
+         DMA_SIZE_G        => DMA_SIZE_C,
+         DMA_AXIS_CONFIG_G => DMA_AXIS_CONFIG_C,
+         AXIL_BASE_ADDR_G  => AXIL_XBAR_CONFIG_C(0).baseAddr)
+      port map (
+         -- Card Management Solution (CMS) Interface
+         cmsHbmCatTrip    => cmsHbmCatTrip,
+         cmsHbmTemp       => cmsHbmTemp,
+         -- HBM Interface
+         userClk          => userClk,
+         hbmRefClk        => hbmRefClk,
+         hbmCatTrip       => hbmCatTrip,
+         -- AXI-Lite Interface (axilClk domain)
+         axilClk          => dmaClk,
+         axilRst          => dmaRst,
+         axilReadMaster   => axilReadMasters(0),
+         axilReadSlave    => axilReadSlaves(0),
+         axilWriteMaster  => axilWriteMasters(0),
+         axilWriteSlave   => axilWriteSlaves(0),
+         -- Trigger Event streams (eventClk domain)
+         eventClk         => (others => dmaClk),
+         eventTrigMsgCtrl => open,
+         -- AXI Stream Interface (axisClk domain)
+         axisClk          => (others => dmaClk),
+         axisRst          => (others => dmaRst),
+         sAxisMasters     => buffIbMasters,
+         sAxisSlaves      => buffIbSlaves,
+         mAxisMasters     => dmaObMasters,
+         mAxisSlaves      => dmaObSlaves);
+
+   ---------------
+   -- PRBS Modules
+   ---------------
+   U_Hardware : entity work.Hardware
+      generic map (
+         TPD_G             => TPD_G,
+         DMA_SIZE_G        => DMA_SIZE_C,
+         NUM_VC_G          => 1,
+         PRBS_SEED_SIZE_G  => 8*DMA_AXIS_CONFIG_C.TDATA_BYTES_C,
+         DMA_AXIS_CONFIG_G => DMA_AXIS_CONFIG_C)
+      port map (
+         -- AXI-Lite Interface
+         axilReadMaster  => axilReadMasters(4),
+         axilReadSlave   => axilReadSlaves(4),
+         axilWriteMaster => axilWriteMasters(4),
+         axilWriteSlave  => axilWriteSlaves(4),
+         -- DMA Interface
+         dmaClk          => dmaClk,
+         dmaRst          => dmaRst,
+         dmaBuffGrpPause => dmaBuffGrpPause,
+         dmaObMasters    => dmaObMasters,
+         dmaObSlaves     => dmaObSlaves,
+         dmaIbMasters    => buffIbMasters,
+         dmaIbSlaves     => buffIbSlaves);
+
+end top_level;
