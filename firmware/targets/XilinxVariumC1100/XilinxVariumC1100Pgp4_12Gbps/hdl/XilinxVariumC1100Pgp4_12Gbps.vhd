39698838ac3cfd0da34e9c7db0e1d2329a5f1065
Larry Ruckman <ruckman@slac.stanford.edu>
2025-06-13 12:08:43 -0700

    adding XilinxVariumC1100Pgp4_12Gbps

--- firmware/targets/XilinxVariumC1100/XilinxVariumC1100Pgp4_12Gbps/hdl/XilinxVariumC1100Pgp4_12Gbps.vhd
+++ firmware/targets/XilinxVariumC1100/XilinxVariumC1100Pgp4_12Gbps/hdl/XilinxVariumC1100Pgp4_12Gbps.vhd
@@ -25,12 +25,11 @@ use surf.Pgp4Pkg.all;
 
 library axi_pcie_core;
 use axi_pcie_core.AxiPciePkg.all;
-use axi_pcie_core.MigPkg.all;
 
 library unisim;
 use unisim.vcomponents.all;
 
-entity XilinxAlveoU200Pgp4_12Gbps is
+entity XilinxVariumC1100Pgp4_12Gbps is
    generic (
       TPD_G                : time                        := 1 ns;
       ROGUE_SIM_EN_G       : boolean                     := false;
@@ -42,51 +41,51 @@ entity XilinxAlveoU200Pgp4_12Gbps is
       --  Application Ports
       ---------------------
       -- QSFP[0] Ports
-      qsfp0RefClkP  : in    slv(1 downto 0);
-      qsfp0RefClkN  : in    slv(1 downto 0);
-      qsfp0RxP      : in    slv(3 downto 0);
-      qsfp0RxN      : in    slv(3 downto 0);
-      qsfp0TxP      : out   slv(3 downto 0);
-      qsfp0TxN      : out   slv(3 downto 0);
+      qsfp0RefClkP : in    sl;
+      qsfp0RefClkN : in    sl;
+      qsfp0RxP     : in    slv(3 downto 0);
+      qsfp0RxN     : in    slv(3 downto 0);
+      qsfp0TxP     : out   slv(3 downto 0);
+      qsfp0TxN     : out   slv(3 downto 0);
       -- QSFP[1] Ports
-      qsfp1RefClkP  : in    slv(1 downto 0);
-      qsfp1RefClkN  : in    slv(1 downto 0);
-      qsfp1RxP      : in    slv(3 downto 0);
-      qsfp1RxN      : in    slv(3 downto 0);
-      qsfp1TxP      : out   slv(3 downto 0);
-      qsfp1TxN      : out   slv(3 downto 0);
-      -- DDR Ports
-      ddrClkP       : in    slv(3 downto 0);
-      ddrClkN       : in    slv(3 downto 0);
-      ddrOut        : out   DdrOutArray(3 downto 0);
-      ddrInOut      : inout DdrInOutArray(3 downto 0);
+      qsfp1RefClkP : in    sl;
+      qsfp1RefClkN : in    sl;
+      qsfp1RxP     : in    slv(3 downto 0);
+      qsfp1RxN     : in    slv(3 downto 0);
+      qsfp1TxP     : out   slv(3 downto 0);
+      qsfp1TxN     : out   slv(3 downto 0);
+      -- HBM Ports
+      hbmCatTrip   : out   sl := '0';  -- HBM Catastrophic Over temperature Output signal to Satellite Controller: active HIGH indicator to Satellite controller to indicate the HBM has exceeds its maximum allowable temperature
       --------------
       --  Core Ports
       --------------
+      -- Card Management Solution (CMS) Interface
+      cmsUartRxd   : in    sl;
+      cmsUartTxd   : out   sl;
+      cmsGpio      : in    slv(3 downto 0);
       -- System Ports
-      userClkP      : in    sl;
-      userClkN      : in    sl;
-      i2cRstL       : out   sl;
-      i2cScl        : inout sl;
-      i2cSda        : inout sl;
-      -- QSFP[1:0] Ports
-      qsfpFs        : out   Slv2Array(1 downto 0);
-      qsfpRefClkRst : out   slv(1 downto 0);
-      qsfpRstL      : out   slv(1 downto 0);
-      qsfpLpMode    : out   slv(1 downto 0);
-      qsfpModSelL   : out   slv(1 downto 0);
-      qsfpModPrsL   : in    slv(1 downto 0);
+      userClkP     : in    sl;
+      userClkN     : in    sl;
+      hbmRefClkP   : in    sl;
+      hbmRefClkN   : in    sl;
+      -- SI5394 Ports
+      si5394Scl    : inout sl;
+      si5394Sda    : inout sl;
+      si5394IrqL   : in    sl;
+      si5394LolL   : in    sl;
+      si5394LosL   : in    sl;
+      si5394RstL   : out   sl;
       -- PCIe Ports
-      pciRstL       : in    sl;
-      pciRefClkP    : in    sl;
-      pciRefClkN    : in    sl;
-      pciRxP        : in    slv(15 downto 0);
-      pciRxN        : in    slv(15 downto 0);
-      pciTxP        : out   slv(15 downto 0);
-      pciTxN        : out   slv(15 downto 0));
-end XilinxAlveoU200Pgp4_12Gbps;
+      pciRstL      : in    sl;
+      pciRefClkP   : in    slv(0 downto 0);
+      pciRefClkN   : in    slv(0 downto 0);
+      pciRxP       : in    slv(7 downto 0);
+      pciRxN       : in    slv(7 downto 0);
+      pciTxP       : out   slv(7 downto 0);
+      pciTxN       : out   slv(7 downto 0));
+end XilinxVariumC1100Pgp4_12Gbps;
 
-architecture top_level of XilinxAlveoU200Pgp4_12Gbps is
+architecture top_level of XilinxVariumC1100Pgp4_12Gbps is
 
    constant AXIL_XBAR_CONFIG_C : AxiLiteCrossbarMasterConfigArray(4 downto 0) := (
       0               => (
@@ -110,7 +109,6 @@ architecture top_level of XilinxAlveoU200Pgp4_12Gbps is
          addrBits     => 23,
          connectivity => x"FFFF"));
 
-   signal userClk156      : sl;
    signal axilClk         : sl;
    signal axilRst         : sl;
    signal axilReadMaster  : AxiLiteReadMasterType;
@@ -133,43 +131,42 @@ architecture top_level of XilinxAlveoU200Pgp4_12Gbps is
    signal buffIbMasters   : AxiStreamMasterArray(7 downto 0);
    signal buffIbSlaves    : AxiStreamSlaveArray(7 downto 0);
 
-   signal ddrClk          : slv(3 downto 0);
-   signal ddrRst          : slv(3 downto 0);
-   signal ddrReady        : slv(3 downto 0);
-   signal ddrWriteMasters : AxiWriteMasterArray(3 downto 0);
-   signal ddrWriteSlaves  : AxiWriteSlaveArray(3 downto 0);
-   signal ddrReadMasters  : AxiReadMasterArray(3 downto 0);
-   signal ddrReadSlaves   : AxiReadSlaveArray(3 downto 0);
+   signal hbmRefClk : sl;
+   signal userClk   : sl;
 
    signal eventTrigMsgCtrl : AxiStreamCtrlArray(7 downto 0) := (others => AXI_STREAM_CTRL_UNUSED_C);
    signal pgpClkOut        : slv(7 downto 0);
    signal pgpTxIn          : Pgp4TxInArray(7 downto 0)      := (others => PGP4_TX_IN_INIT_C);
 
+   signal cmsHbmCatTrip : sl                    := '0';
+   signal cmsHbmTemp    : Slv7Array(1 downto 0) := (others => b"0000000");
+
 begin
 
    U_axilClk : entity surf.ClockManagerUltraScale
       generic map(
-         TPD_G             => TPD_G,
-         TYPE_G            => "PLL",
-         INPUT_BUFG_G      => true,
-         FB_BUFG_G         => true,
-         RST_IN_POLARITY_G => '1',
-         NUM_CLOCKS_G      => 1,
+         TPD_G              => TPD_G,
+         TYPE_G             => "MMCM",
+         INPUT_BUFG_G       => true,
+         FB_BUFG_G          => true,
+         RST_IN_POLARITY_G  => '1',
+         NUM_CLOCKS_G       => 1,
          -- MMCM attributes
-         BANDWIDTH_G       => "OPTIMIZED",
-         CLKIN_PERIOD_G    => 6.4,      -- 156.25 MHz
-         CLKFBOUT_MULT_G   => 8,        -- 1.25GHz = 8 x 156.25 MHz
-         CLKOUT0_DIVIDE_G  => 8)        -- 156.25MHz = 1.25GHz/8
+         BANDWIDTH_G        => "OPTIMIZED",
+         CLKIN_PERIOD_G     => 10.0,    -- 100MHz
+         DIVCLK_DIVIDE_G    => 8,       -- 12.5MHz = 100MHz/8
+         CLKFBOUT_MULT_F_G  => 96.875,  -- 1210.9375MHz = 96.875 x 12.5MHz
+         CLKOUT0_DIVIDE_F_G => 7.75)    -- 156.25MHz = 1210.9375MHz/7.75
       port map(
          -- Clock Input
-         clkIn     => userClk156,
+         clkIn     => userClk,
          rstIn     => dmaRst,
          -- Clock Outputs
          clkOut(0) => axilClk,
          -- Reset Outputs
          rstOut(0) => axilRst);
 
-   U_Core : entity axi_pcie_core.XilinxAlveoU200Core
+   U_Core : entity axi_pcie_core.XilinxVariumC1100Core
       generic map (
          TPD_G                => TPD_G,
          QSFP_CDR_DISABLE_G   => false,  -- FALSE: 25.78125Gbps/12.5Gbps = 2.0625 = 2 + 1/16
@@ -182,7 +179,8 @@ begin
          ------------------------
          --  Top Level Interfaces
          ------------------------
-         userClk156      => userClk156,
+         userClk         => userClk,
+         hbmRefClk       => hbmRefClk,
          -- DMA Interfaces
          dmaClk          => dmaClk,
          dmaRst          => dmaRst,
@@ -191,7 +189,7 @@ begin
          dmaObSlaves     => dmaObSlaves,
          dmaIbMasters    => dmaIbMasters,
          dmaIbSlaves     => dmaIbSlaves,
-         -- AXI-Lite Interface
+         -- Application AXI-Lite Interfaces [0x00100000:0x00FFFFFF]
          appClk          => axilClk,
          appRst          => axilRst,
          appReadMaster   => axilReadMaster,
@@ -201,19 +199,24 @@ begin
          --------------
          --  Core Ports
          --------------
+         -- Card Management Solution (CMS) Interface
+         cmsHbmCatTrip   => cmsHbmCatTrip,
+         cmsHbmTemp      => cmsHbmTemp,
+         cmsUartRxd      => cmsUartRxd,
+         cmsUartTxd      => cmsUartTxd,
+         cmsGpio         => cmsGpio,
          -- System Ports
          userClkP        => userClkP,
          userClkN        => userClkN,
-         i2cRstL         => i2cRstL,
-         i2cScl          => i2cScl,
-         i2cSda          => i2cSda,
-         -- QSFP[1:0] Ports
-         qsfpFs          => qsfpFs,
-         qsfpRefClkRst   => qsfpRefClkRst,
-         qsfpRstL        => qsfpRstL,
-         qsfpLpMode      => qsfpLpMode,
-         qsfpModSelL     => qsfpModSelL,
-         qsfpModPrsL     => qsfpModPrsL,
+         hbmRefClkP      => hbmRefClkP,
+         hbmRefClkN      => hbmRefClkN,
+         -- SI5394 Ports
+         si5394Scl       => si5394Scl,
+         si5394Sda       => si5394Sda,
+         si5394IrqL      => si5394IrqL,
+         si5394LolL      => si5394LolL,
+         si5394LosL      => si5394LosL,
+         si5394RstL      => si5394RstL,
          -- PCIe Ports
          pciRstL         => pciRstL,
          pciRefClkP      => pciRefClkP,
@@ -244,38 +247,23 @@ begin
          mAxiReadMasters     => axilReadMasters,
          mAxiReadSlaves      => axilReadSlaves);
 
-   --------------------
-   -- MIG[3:0] IP Cores
-   --------------------
-   U_Mig : entity axi_pcie_core.MigAll
-      generic map (
-         TPD_G => TPD_G)
-      port map (
-         extRst          => dmaRst,
-         -- AXI MEM Interface
-         axiClk          => ddrClk,
-         axiRst          => ddrRst,
-         axiReady        => ddrReady,
-         axiWriteMasters => ddrWriteMasters,
-         axiWriteSlaves  => ddrWriteSlaves,
-         axiReadMasters  => ddrReadMasters,
-         axiReadSlaves   => ddrReadSlaves,
-         -- DDR Ports
-         ddrClkP         => ddrClkP,
-         ddrClkN         => ddrClkN,
-         ddrOut          => ddrOut,
-         ddrInOut        => ddrInOut);
-
    ----------------------------
    -- DMA Inbound Large Buffer
    ----------------------------
-   U_MigDmaBuffer : entity axi_pcie_core.MigDmaBuffer
+   U_HbmDmaBuffer : entity axi_pcie_core.HbmDmaBuffer
       generic map (
          TPD_G             => TPD_G,
          DMA_SIZE_G        => 8,
          DMA_AXIS_CONFIG_G => DMA_AXIS_CONFIG_G,
          AXIL_BASE_ADDR_G  => AXIL_XBAR_CONFIG_C(0).baseAddr)
       port map (
+         -- Card Management Solution (CMS) Interface
+         cmsHbmCatTrip    => cmsHbmCatTrip,
+         cmsHbmTemp       => cmsHbmTemp,
+         -- HBM Interface
+         userClk          => userClk,
+         hbmRefClk        => hbmRefClk,
+         hbmCatTrip       => hbmCatTrip,
          -- AXI-Lite Interface (axilClk domain)
          axilClk          => axilClk,
          axilRst          => axilRst,
@@ -284,32 +272,18 @@ begin
          axilWriteMaster  => axilWriteMasters(0),
          axilWriteSlave   => axilWriteSlaves(0),
          -- Trigger Event streams (eventClk domain)
-         eventClk         => axilClk,
+         eventClk         => pgpClkOut,
          eventTrigMsgCtrl => eventTrigMsgCtrl,
          -- AXI Stream Interface (axisClk domain)
-         axisClk          => dmaClk,
-         axisRst          => dmaRst,
+         axisClk          => (others => dmaClk),
+         axisRst          => (others => dmaRst),
          sAxisMasters     => buffIbMasters,
          sAxisSlaves      => buffIbSlaves,
          mAxisMasters     => dmaIbMasters,
-         mAxisSlaves      => dmaIbSlaves,
-         -- DDR AXI MEM Interface
-         ddrClk           => ddrClk,
-         ddrRst           => ddrRst,
-         ddrReady         => ddrReady,
-         ddrWriteMasters  => ddrWriteMasters,
-         ddrWriteSlaves   => ddrWriteSlaves,
-         ddrReadMasters   => ddrReadMasters,
-         ddrReadSlaves    => ddrReadSlaves);
+         mAxisSlaves      => dmaIbSlaves);
 
    GEN_LANE : for i in 7 downto 0 generate
-      U_remoteDmaPause : entity surf.Synchronizer
-         generic map (
-            TPD_G => TPD_G)
-         port map (
-            clk     => pgpClkOut(i),
-            dataIn  => eventTrigMsgCtrl(i).pause,
-            dataOut => pgpTxIn(i).locData(0));
+      pgpTxIn(i).locData(0) <= eventTrigMsgCtrl(i).pause;
    end generate;
 
    U_Hardware : entity work.Hardware
