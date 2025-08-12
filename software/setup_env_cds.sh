37ba09b5675124e0bb0cd5e9018a3f6e41824b07
Larry Ruckman <ruckman@slac.stanford.edu>
2025-08-12 09:30:11 -0700

    adding setup_env_cds.sh

---
+++ software/setup_env_cds.sh
@@ -0,0 +1,9 @@
+##################################
+# Setup environment
+##################################
+source /cds/group/pcds/pyps/conda/rogue/etc/profile.d/conda.sh
+
+##################################
+# Activate Rogue conda Environment
+##################################
+conda activate rogue_v6.5.0
