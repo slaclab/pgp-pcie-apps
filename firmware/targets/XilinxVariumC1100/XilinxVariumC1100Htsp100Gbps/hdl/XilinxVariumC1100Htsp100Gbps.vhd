c8441770583bc77840d793e888e409a55da09901
Larry Ruckman <ruckman@slac.stanford.edu>
2025-09-30 13:21:26 -0700

    HbmDmaBufferV2 API updates

--- firmware/targets/XilinxVariumC1100/XilinxVariumC1100Htsp100Gbps/hdl/XilinxVariumC1100Htsp100Gbps.vhd
+++ firmware/targets/XilinxVariumC1100/XilinxVariumC1100Htsp100Gbps/hdl/XilinxVariumC1100Htsp100Gbps.vhd
@@ -268,11 +268,14 @@ begin
          -- Trigger Event streams (eventClk domain)
          eventClk         => htspClkOut,
          eventTrigMsgCtrl => eventTrigMsgCtrl,
-         -- AXI Stream Interface (axisClk domain)
-         axisClk          => (others => dmaClk),
-         axisRst          => (others => dmaRst),
+         -- Inbound AXIS Interface (sAxisClk domain)
+         sAxisClk         => (others => dmaClk),
+         sAxisRst         => (others => dmaRst),
          sAxisMasters     => buffIbMasters,
          sAxisSlaves      => buffIbSlaves,
+         -- Outbound AXIS Interface (sAxisClk domain)
+         mAxisClk         => (others => dmaClk),
+         mAxisRst         => (others => dmaRst),
          mAxisMasters     => dmaIbMasters,
          mAxisSlaves      => dmaIbSlaves);
 
